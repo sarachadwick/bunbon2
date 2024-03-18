bb789be95dd1efb14acb21771e5c5ea5c1e930ce
Sara Chadwick <sarachadwick92@gmail.com>
2024-03-18 14:00:58 -0500

    Add a second incrementer

--- src/routes/+page.server.js
+++
@@ -1,4 +0,0 @@
-export function load({ cookies }) {
-  const muffins = cookies.get('muffins') || 0; 
-  return { muffins };
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,26 @@
   import muffinImg from '$lib/assets/simpleMuffin.png';
   import cottontailRabbitImg from '$lib/assets/simpleCottontail.png';
 
-  //export let data;
   let muffins: number = browser ? parseInt(window.localStorage.getItem('muffins')) || 0 : 0;
-  let bunNum: number = browser ? parseInt(window.localStorage.getItem('bunNum')) || 0 : 0;
-  let bunnyCost: number = 5;
+  let cookies: number = browser ? parseInt(window.localStorage.getItem('cookies')) || 0 : 0;
+  let cottontailBunNum: number = browser
+    ? parseInt(window.localStorage.getItem('cottontailBunNum')) || 0
+    : 0;
+  let hollandLopBunNum: number = browser
+    ? parseInt(window.localStorage.getItem('hollandLopBunNum')) || 0
+    : 0;
+  let cottontailBunnyCost: number = 5;
+  let hollandLopBunnyCost: number = 20;
   let interval;
   let shouldHideCottontail: boolean = muffins <= 3;
+  let shouldHideHollandLopBunny: boolean = cookies <= 20;
+  let shouldHideCookies: boolean = muffins <= 20;
 
-  let increment = () => {
-    muffins += bunNum;
+  let muffinIncrement = () => {
+    muffins += cottontailBunNum;
+  };
+  let cookieIncrement = () => {
+    cookies += hollandLopBunNum;
   };
 
   let makeAMuffin = () => {
@@ -19,13 +30,31 @@
     if (muffins > 3) {
       shouldHideCottontail = false;
     }
+    if (muffins > 20) {
+      shouldHideCookies = false;
+    }
+  };
+
+  let makeACookie = () => {
+    cookies += 1;
+    if (cookies > 20) {
+      shouldHideHollandLopBunny = false;
+    }
   };
 
   let buyCottontailRabbit = () => {
-    if (muffins >= bunnyCost) {
-      muffins -= bunnyCost;
-      bunnyCost = Math.trunc(1.2 * bunnyCost);
-      bunNum += 1;
+    if (muffins >= cottontailBunnyCost) {
+      muffins -= cottontailBunnyCost;
+      cottontailBunnyCost = Math.trunc(1.2 * cottontailBunnyCost);
+      cottontailBunNum += 1;
+    }
+  };
+
+  let buyHollandLop = () => {
+    if (cookies >= hollandLopBunnyCost) {
+      cookies -= hollandLopBunnyCost;
+      hollandLopBunnyCost = Math.trunc(1.2 * hollandLopBunnyCost);
+      hollandLopBunNum += 1;
     }
   };
 
@@ -37,29 +66,82 @@
   }
 
   $: {
-    bunNum;
-    if (bunNum != 0) {
-      interval = setInterval(increment, 1000);
+    cookies;
+    if (browser) {
+      window.localStorage.setItem('cookies', muffins.toString());
+    }
+  }
+
+  $: {
+    cottontailBunNum;
+    if (cottontailBunNum != 0) {
+      interval = setInterval(muffinIncrement, 1000);
     }
     if (browser) {
-      window.localStorage.setItem('bunNum', bunNum.toString());
+      window.localStorage.setItem('cottontailBunNum', cottontailBunNum.toString());
+    }
+  }
+
+  $: {
+    hollandLopBunNum;
+    if (hollandLopBunNum != 0) {
+      interval = setInterval(cookieIncrement, 1000);
+    }
+    if (browser) {
+      window.localStorage.setItem('hollandLopBunNum', hollandLopBunNum.toString());
     }
   }
 </script>
 
 {#if browser}
-  <button on:click={makeAMuffin}>
-    <img src={muffinImg} alt="the muffin-makin muffin" height="100" width="100" />
-  </button>
-  <p class="muffin-instructions">Click a muffin, make a muffin</p>
-  <p class="muffin-count">{muffins}</p>
-  <div class="cottontail-rabbit" class:hidden={shouldHideCottontail}>
-    <button on:click={buyCottontailRabbit}>
-      <img src={cottontailRabbitImg} alt="buy a bunny" height="200" width="200" />
-    </button>
-    <p>
-      Convince a cottontail bunny to join you: {bunnyCost} muffins
-    </p>
+  <div style="display:flex; justify-content:space-evenly">
+    <div id="muffin-stuffs" style="display: flex; align-items: center; flex-direction:column;">
+      <p class="muffin-instructions">Click a muffin, make a muffin</p>
+      <p>Muffins:</p>
+      <p class="muffin-count">{muffins}</p>
+      <span class:hidden={shouldHideCookies}>
+        <p>Cookies:</p>
+        <p class="muffin-count">{cookies}</p>
+      </span>
+    </div>
+    <div id="clickables">
+      <div style="display: flex; justify-content: center;">
+        <button on:click={makeAMuffin}>
+          <img src={muffinImg} alt="the muffin-makin muffin" height="100" width="100" />
+        </button>
+        <span class:hidden={shouldHideCookies}>
+          <button on:click={makeACookie}>
+            <img src={muffinImg} alt="clicking of the cookie" height="100" width="100" />
+          </button>
+        </span>
+      </div>
+      <div style="display: flex; justify-content: center; align">
+        <div
+          class="cottontail-rabbit"
+          class:hidden={shouldHideCottontail}
+          style="display: flex; justify-content: center; flex-direction:column;"
+        >
+          <button on:click={buyCottontailRabbit} style="display: flex; justify-content: center;">
+            <img src={cottontailRabbitImg} alt="buy a bunny" height="200" width="200" />
+          </button>
+          <p>
+            Convince a cottontail bunny to join you: {cottontailBunnyCost} muffins
+          </p>
+        </div>
+        <div
+          class="holland-lop-rabbit"
+          class:hidden={shouldHideHollandLopBunny}
+          style="display: flex; justify-content: center; flex-direction:column;"
+        >
+          <button on:click={buyHollandLop} style="display: flex; justify-content: center;">
+            <img src={cottontailRabbitImg} alt="buy a holland lop" height="200" width="200" />
+          </button>
+          <p>
+            Convince a cottontail bunny to join you: {hollandLopBunnyCost} cookies
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 {:else}
   Loading muffins...
